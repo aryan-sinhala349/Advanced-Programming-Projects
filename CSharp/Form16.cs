900baf2252d415d11d93068f6cf27a8af5a258cf
Aryan <46282971+aryan-sinhala349@users.noreply.github.com>
2020-02-18 14:18:34 -0500

    Added Form 16
    I had to tell Jack that github is good for C#

---
+++ CSharp/Form16.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdvancedProgrammingCSharp
+{
+    public partial class Form16 : Form
+    {
+        Random r = new Random();
+
+        public Form16()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(generate(r.Next(104)).ToString() + " was your selected seed!", "Random Seed Generator");
+        }
+
+        private int generate(int tries)
+        {
+            MessageBox.Show("You have " + tries + " tries", "Random Seed Generator");
+
+            int[] values = new int[tries];
+
+            for (int i = 0; i < tries; i++)
+            {
+                int value = r.Next();
+                values[i] = value;
+            }
+
+            MessageBox.Show(string.Join(", ", values), "Possible Seeds");
+
+            int result = r.Next(tries);
+            return values[result];
+        }
+    }
+}
