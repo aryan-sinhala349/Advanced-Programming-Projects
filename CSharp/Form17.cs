cd935fe12a4fe94ac197247008e0db27c20ce310
Aryan <46282971+aryan-sinhala349@users.noreply.github.com>
2020-02-26 13:53:25 -0500

    Added Form 17
    I think this is more advanced than what was in the video.

---
+++ CSharp/Form17.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdvancedProgrammingCSharp
+{
+    public partial class Form17 : Form
+    {
+        Random r = new Random();
+
+        public Form17()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int i = r.Next(10);
+            MessageBox.Show(Odd(i) ? "Yes." : "No.", "Is " + i + " odd?");
+        }
+        
+        bool Odd(int value)
+        {
+            return value % 2 == 1;
+        }
+    }
+}
