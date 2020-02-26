75eae2be5c93563d4cd22a3c0c6fc0462b34d010
Aryan <46282971+aryan-sinhala349@users.noreply.github.com>
2020-02-26 14:11:07 -0500

    Added Form 19 and Tutorial 19 Class
    This got a little bit more interesting, but I still think that what I'm doing is a little bit extra.

---
+++ CSharp/Form19.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Sinha.Tutorial19Namespace;
+
+namespace AdvancedProgrammingCSharp
+{
+    public partial class Form19 : Form
+    {
+        public Form19()
+        {
+            InitializeComponent();
+        }
+
+        private void Form19_Load(object sender, EventArgs e)
+        {
+            Tutorial19Class.ShowMessage("This is a message", "Tutorial 19 Class");
+        }
+    }
+}
---
+++ CSharp/Tutorial19Class.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Forms;
+
+namespace Sinha
+{
+    namespace Tutorial19Namespace
+    {
+        class Tutorial19Class
+        {
+            public static void ShowMessage(string message, string caption)
+            {
+                MessageBox.Show(message, caption, MessageBoxButtons.OK, MessageBoxIcon.None, MessageBoxDefaultButton.Button1);
+            }
+        }
+    }
+}
