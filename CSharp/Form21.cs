d5ac800630bdaa0bfc7970b41a932bb0fd05a14e
Aryan <46282971+aryan-sinhala349@users.noreply.github.com>
2020-02-26 14:29:55 -0500

    Added Form 21
    This is starting to get real interesting... I think.

---
+++ CSharp/Form21.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Sinha.Tutorial21;
+
+namespace AdvancedProgrammingCSharp
+{
+    public partial class Form21 : Form
+    {
+        public Form21()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Tutorial21Class t21c = new Tutorial21Class("Aryan");
+            MessageBox.Show(t21c.name);
+            Tutorial21Class.HowManyNames();
+        }
+    }
+}
---
+++ CSharp/Tutorial21Class.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sinha
+{
+    namespace Tutorial21
+    {
+        class Tutorial21Class
+        {
+            public string name { get; set; }
+            public static int numNames { get; private set; }
+
+            public Tutorial21Class(string name = "")
+            {
+                this.name = name;
+                ++numNames;
+            }
+
+            public static void HowManyNames()
+            {
+                MessageBox.Show(numNames.ToString(), "You have created this many names");
+            }
+        }
+    }
+}
