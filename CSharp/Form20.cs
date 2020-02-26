ebb8d0785a1f1f1e044beb0b5dfaa68ed3d7967e
Aryan <46282971+aryan-sinhala349@users.noreply.github.com>
2020-02-26 14:18:30 -0500

    Added Form 20
    I don't know man, this is pretty similar to Java.

---
+++ CSharp/Form20.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Sinha.Tutorial20;
+
+namespace AdvancedProgrammingCSharp
+{
+    public partial class Form20 : Form
+    {
+        public Form20()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Tutorial20Class t2c = new Tutorial20Class("Aryan");
+
+            MessageBox.Show(t2c.name, "What is my name?");
+        }
+    }
+}
---
+++ CSharp/Tutorial20Class.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sinha
+{
+    namespace Tutorial20
+    {
+        class Tutorial20Class
+        {
+            public string name { get; set; }
+
+            public Tutorial20Class(string name = "I don't know.")
+            {
+                this.name = name;
+            }
+        }
+    }
+}
