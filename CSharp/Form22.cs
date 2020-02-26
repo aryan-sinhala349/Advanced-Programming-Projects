9235734db7c8c496d8a57a289e57eac4aa96be75
Aryan <46282971+aryan-sinhala349@users.noreply.github.com>
2020-02-26 14:45:22 -0500

    Form 22
    I have so much energy

---
+++ CSharp/Form22.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Sinha.Tutorial22;
+
+namespace AdvancedProgrammingCSharp
+{
+    public partial class Form22 : Form
+    {
+        public Form22()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Tutorial22Class.ShowMessage("This is without a title");
+            Tutorial22Class.ShowMessage("This is with a title", "I've put a title here");
+        }
+    }
+}
---
+++ CSharp/Tutorial22Class.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sinha
+{
+    namespace Tutorial22
+    {
+        static class Tutorial22Class
+        {
+            public static void ShowMessage(string message)
+            {
+                MessageBox.Show(message, "This is a default title");
+            }
+
+            public static void ShowMessage(string message, string caption)
+            {
+                MessageBox.Show(message, caption);
+            }
+        }
+    }
+}
