5082d922819f49001b37ce9bb0123e3b9567a2f5
Aryan <46282971+aryan-sinhala349@users.noreply.github.com>
2020-02-28 14:17:52 -0500

    Added Form 24
    The guy sitting next to me and I were arguing about framerules in Mario. Just say that I, the guy who used to speedrun the game and literally has a mario texture as his profile picture, am correct.

---
+++ CSharp/Form24.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdvancedProgrammingCSharp
+{
+    public partial class Form24 : Form
+    {
+        public Form24()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string text = textBox1.Text;
+
+            Tutorial24Class.CheckString(text);
+
+            MessageBox.Show("You typed \"" + text + "\", which was a valid string!");
+        }
+    }
+}
---
+++ CSharp/Tutorial24Class.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedProgrammingCSharp
+{
+    class Tutorial24Class
+    {
+        private static Exception emptyString = new Exception("That is an empty string!");
+
+        public static void CheckString(string value)
+        {
+            if (value == "") throw emptyString;
+        }
+    }
+}
