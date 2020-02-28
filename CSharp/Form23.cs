b900696bedb572a96dd60f7744f5505a7d2b2a76
Aryan <46282971+aryan-sinhala349@users.noreply.github.com>
2020-02-28 14:06:30 -0500

    Added Form 23
    Is anyone reading these, or am I just wasting my time?

---
+++ CSharp/Form23.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdvancedProgrammingCSharp
+{
+    public partial class Form23 : Form
+    {
+        public Form23()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Tutorial23Class tutorial = new Tutorial23Class("Aryan");
+            MessageBox.Show(tutorial.Name);
+            tutorial.Name = "";
+        }
+    }
+}
---
+++ CSharp/Tutorial23Class.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdvancedProgrammingCSharp
+{
+    class Tutorial23Class
+    {
+        private string name;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                if (value == "") MessageBox.Show("You can't set the name to \"\"", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                name = value;
+            }
+        }
+
+        public Tutorial23Class(string name)
+        {
+            this.name = name;
+        }
+    }
+}
