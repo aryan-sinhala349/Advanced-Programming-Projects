700542726fee5450b27bb1e4f3871a39249b4eaf
Aryan <46282971+aryan-sinhala349@users.noreply.github.com>
2020-03-06 14:24:07 -0500

    Added Form 28
    The Oingo Boingo brothers are, hands down, the best Jojo characters ever.

---
+++ CSharp/Client.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNamespace
+{
+    struct Client : IClient
+    {
+        public string Name;
+        public int Age;
+
+        public Client(string name = "Adam", int age = 15)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public void ClearClientInfo()
+        {
+            Name = "";
+            Age = 0;
+        }
+    }
+
+    interface IClient
+    {
+        void ClearClientInfo();
+    }
+}
---
+++ CSharp/Form28.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MyNamespace;
+
+namespace AdvancedProgrammingCSharp
+{
+    public partial class Form28 : Form
+    {
+        Client[] clients = { new Client("Adam", 15), new Client("Jotaro Kujo", 17), new Client("Giorno Giovanna", 15), new Client("Noriaki Kakyoin", 17) };
+
+        public Form28()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < clients.Length; ++i)
+                MessageBox.Show(clients[i].Name + ", " + clients[i].Age, "Client " + (i + 1));
+
+            for (int i = 0; i < clients.Length; ++i)
+                clients[i].ClearClientInfo();
+
+            for (int i = 0; i < clients.Length; ++i)
+                MessageBox.Show(clients[i].Name + ", " + clients[i].Age, "Client " + (i + 1));
+        }
+    }
+}
