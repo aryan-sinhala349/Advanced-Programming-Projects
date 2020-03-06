334f4dce3ab4d3333351c9316857c8a6edcf2b81
Aryan <46282971+aryan-sinhala349@users.noreply.github.com>
2020-03-06 14:42:09 -0500

    Added Form 29
    I'm losing focus...

---
+++ CSharp/Human.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyNamespace
+{
+    partial class Human
+    {
+        public string Name = "Mr. Smith";
+        public int Age = 38;
+
+        partial void Message(string message);
+    }
+
+    partial class Human
+    {
+        public string HairColor = "Brown";
+        public bool Glasses = false;
+
+        partial void Message(string message)
+        {
+            MessageBox.Show(message, Name);
+        }
+
+        public void ShowMessage(string message)
+        {
+            Message(message);
+        }
+    }
+}
