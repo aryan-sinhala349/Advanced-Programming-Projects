212c411d3bde1a36fa2fd28dddcc627a27463893
Aryan <46282971+aryan-sinhala349@users.noreply.github.com>
2020-03-06 13:56:29 -0500

    Added Form 26

---
+++ CSharp/Form26.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using People;
+
+namespace AdvancedProgrammingCSharp
+{
+    public partial class Form26 : Form
+    {
+        Person person = new Person();
+        Worker worker = new Worker();
+        IPolitician politician = new Politician();
+
+        public Form26()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            person.ShowInfo();
+            worker.ShowInfo();
+            politician.Advertise();
+        }
+    }
+}
