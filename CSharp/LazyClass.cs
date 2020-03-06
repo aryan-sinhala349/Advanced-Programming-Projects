cad713dc0e1c729a3031e5bb94503ef245efecc3
Aryan <46282971+aryan-sinhala349@users.noreply.github.com>
2020-03-06 14:51:33 -0500

    Added Form 30
    Not saying that Adams are lazy, but instead that I'm too lazy to create a class name.

---
+++ CSharp/LazyClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyNamespace
+{
+    abstract class LazyClass
+    {
+        public static string Name = "Adam";
+        public static int Age = 15;
+
+        public static void Message(string message)
+        {
+            MessageBox.Show(message);
+        }
+
+        public abstract void ShowMessage(string message);
+    }
+
+    class LazyClassTwo : LazyClass
+    {
+        public override void ShowMessage(string message)
+        {
+            Message(message);
+        }
+    }
+}
