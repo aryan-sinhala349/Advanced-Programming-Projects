96d3c804162d4df5757958b318934aa0583580a6
Aryan <46282971+aryan-sinhala349@users.noreply.github.com>
2020-03-10 14:50:43 -0400

    Added Form 35
    Not gonna lie, this should have been an earlier episode. Going over different variable types 35 episodes in is NOT the move.

---
+++ CSharp/Form35.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdvancedProgrammingCSharp
+{
+    public partial class Form35 : Form
+    {
+        public Form35()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            byte myByte = 255;
+            short myShort = 0;
+            Int16 int16 = myShort;
+            uint myInt = 0;
+            UInt32 myInt32 = myInt;
+            ulong myLong = 0;
+            UInt64 myInt64 = myLong;
+
+            float myFloat = 0.5f;
+            double d = 3.14159265358979;
+
+            char c = 'd';
+        }
+    }
+}
