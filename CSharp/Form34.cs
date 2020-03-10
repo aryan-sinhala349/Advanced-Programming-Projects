9fce3375dc6ad331c6546ef054739b365016bf70
Aryan <46282971+aryan-sinhala349@users.noreply.github.com>
2020-03-10 14:42:51 -0400

    Added Form 34
    What should I put here?

---
+++ CSharp/Form34.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdvancedProgrammingCSharp
+{
+    public partial class Form34 : Form
+    {
+        public Form34()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Title Image";
+            ofd.Filter = "PNG Image|*.png";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = ofd.FileName;
+                MessageBox.Show(ofd.SafeFileName);
+            }
+            else MessageBox.Show("Please select an image!", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+}
