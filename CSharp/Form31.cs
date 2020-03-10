494c3f8b90c71fbf344242af857914b2743b358b
Aryan <46282971+aryan-sinhala349@users.noreply.github.com>
2020-03-10 13:49:29 -0400

    Added Form 31
    We may have a situation involving COVID-19 or Coronavirus.

---
+++ CSharp/Form31.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdvancedProgrammingCSharp
+{
+    public partial class Form31 : Form
+    {
+        MyNamespace.Message message = new MyNamespace.Message();
+
+        public Form31()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            message.ShowThoseMessages();
+        }
+    }
+}
---
+++ CSharp/Message.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyNamespace
+{
+    class Message
+    {
+        delegate void MyDelegate(string myString);
+
+        public void ShowThoseMessages()
+        {
+            MyDelegate md = new MyDelegate(ShowMessage);
+            md += ShowAnotherMessage;
+            md("Adam");
+        }
+
+        public void ShowMessage(string message)
+        {
+            MessageBox.Show(message);
+        }
+
+        public void ShowAnotherMessage(string message)
+        {
+            MessageBox.Show(message, "Test");
+        }
+    }
+}
