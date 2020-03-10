48c162150069398060de2d452eb45f05acf46e9d
Aryan <46282971+aryan-sinhala349@users.noreply.github.com>
2020-03-10 14:13:43 -0400

    Added Form 33
    I think I did at least one ternary operator in one of the previous forms.

---
+++ CSharp/Form33.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdvancedProgrammingCSharp
+{
+    public partial class Form33 : Form
+    {
+        public Form33()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string myString = checkBox1.Checked ? "It's checked." : "It's not checked.";
+            MessageBox.Show(myString);
+        }
+    }
+}
