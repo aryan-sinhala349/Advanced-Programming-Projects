05c49359dfb6e000c4cf04dd75ba770e70af318d
Aryan <46282971+aryan-sinhala349@users.noreply.github.com>
2020-03-12 14:11:58 -0400

    Added Forms 38 and 39
    I completely forgot to add Form 38. This is unacceptable. Because of this, I will have to suspend development of Futon Fight indefinitely.

---
+++ CSharp/Form38.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace AdvancedProgrammingCSharp
+{
+    public partial class Form38 : Form
+    {
+        public Form38()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader sr = new StreamReader(ofd.FileName);
+                char c = (char)sr.Peek();
+                char c1 = (char)sr.Read();
+                char c2 = (char)sr.Read();
+                MessageBox.Show(c.ToString() + " : " + c1.ToString() + " : " + c2.ToString());
+            }
+        }
+    }
+}
---
+++ CSharp/Form39.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace AdvancedProgrammingCSharp
+{
+    public partial class Form39 : Form
+    {
+        string path;
+
+        public Form39()
+        {
+            InitializeComponent();
+        }
+
+        private void Form39_Load(object sender, EventArgs e)
+        {
+            button2.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                button2.Enabled = true;
+                path = ofd.FileName;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            StreamWriter sw = new StreamWriter(File.Create(path));
+            sw.WriteLine(textBox1.Text);
+            sw.Dispose();
+        }
+    }
+}
