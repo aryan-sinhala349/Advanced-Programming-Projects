5c8f8aa68439506e3988b697e3785e95f54b5399
Aryan <46282971+aryan-sinhala349@users.noreply.github.com>
2020-03-12 13:50:51 -0400

    Added Form 37
    People are now playing Chess. The person in front of me has no clue what he's doing. These are scary times.

---
+++ CSharp/Form37.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace AdvancedProgrammingCSharp
+{
+    public partial class Form37 : Form
+    {
+        public Form37()
+        {
+            InitializeComponent();
+        }
+
+        private void Form37_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader sr = new StreamReader(File.OpenRead(ofd.FileName));
+                sr.BaseStream.Position = 0x0C;
+                byte[] buffer = new byte[3];
+                sr.BaseStream.Read(buffer, 0, 3);
+                foreach (byte b in buffer)
+                    textBox1.Text += b.ToString("x") + " ";
+                sr.Dispose();
+            }
+        }
+    }
+}
