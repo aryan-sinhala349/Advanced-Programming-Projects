de806c8d4bd5674db9a2222e98986c03260ca930
Aryan <46282971+aryan-sinhala349@users.noreply.github.com>
2020-03-12 14:30:18 -0400

    Added Form 41
    We are so closed to finished. I am almost cool, right?

---
+++ CSharp/Form41.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace AdvancedProgrammingCSharp
+{
+    public partial class Form41 : Form
+    {
+        string path;
+
+        public Form41()
+        {
+            InitializeComponent();
+        }
+
+        private void Form41_Load(object sender, EventArgs e)
+        {
+            button2.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                button2.Enabled = true;
+                path = ofd.FileName;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            BinaryReader br = new BinaryReader(File.OpenRead(path));
+            br.BaseStream.Position = 0x1E;
+            textBox1.Text = br.ReadInt16().ToString("X");
+            br.Dispose();
+        }
+    }
+}
