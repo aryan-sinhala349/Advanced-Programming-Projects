eebb2c256ddccf0e03b92a77207afee3ace1ba3f
Aryan <46282971+aryan-sinhala349@users.noreply.github.com>
2020-03-12 13:40:11 -0400

    Added Form 36
    People were playing Tetris while I was completing this.

---
+++ CSharp/Form36.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace AdvancedProgrammingCSharp
+{
+    public partial class Form36 : Form
+    {
+        public Form36()
+        {
+            InitializeComponent();
+        }
+
+        private void Form36_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader sr = new StreamReader(File.OpenRead(ofd.FileName));
+                textBox1.Text = sr.ReadToEnd();
+                sr.Dispose();
+            }
+        }
+    }
+}
