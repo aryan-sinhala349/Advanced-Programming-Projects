d52d513b81428c9bd03203bffec8b71278a483ad
Aryan <46282971+aryan-sinhala349@users.noreply.github.com>
2020-03-14 16:11:56 -0400

    Added Form 47
    Tutorial 46 is a copy of tutorial 45. Therefore, the script files are the same, and I'm not willing to type out the exact same code twice, which is a waste of space.

---
+++ CSharp/Form47.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdvancedProgrammingCSharp
+{
+    public partial class Form47 : Form
+    {
+        public Form47()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Control myControl = button1;
+            object myObj = "Hello";
+            if (myObj is string)
+            {
+                string myString = myObj as string;
+                MessageBox.Show(myString);
+            }
+        }
+    }
+}
