fef657254adffe19d05816277e3343544d04c3a1
Aryan <46282971+aryan-sinhala349@users.noreply.github.com>
2020-03-14 16:33:49 -0400

    Added Form 52
    This is a pain to debug, because you have to become a Ness main and spam okay 26 times.

---
+++ CSharp/Form52.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdvancedProgrammingCSharp
+{
+    public partial class Form52 : Form
+    {
+        public Form52()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string alphabet = "abcdefghijklmnopqrstuvwxyz";
+            char[] letters = alphabet.ToCharArray();
+            foreach (char c in letters)
+                MessageBox.Show(c.ToString());
+        }
+    }
+}
