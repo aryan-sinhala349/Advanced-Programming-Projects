731129dc9f58a702f68da823081144bdbdbf6ec3
Aryan <46282971+aryan-sinhala349@users.noreply.github.com>
2020-03-14 15:55:29 -0400

    Added Form 44
    I would say that funny machine broke, but funny machine never worked.

---
+++ CSharp/Form44.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace AdvancedProgrammingCSharp
+{
+    public partial class Form44 : Form
+    {
+        public Form44()
+        {
+            InitializeComponent();
+        }
+        private void Form44_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Text File|*.txt";
+            sfd.FileName = "My Text File";
+            sfd.Title = "Save Text File";
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                string path = sfd.FileName;
+                BinaryWriter bw = new BinaryWriter(File.Create(path));
+                bw.Write("Example text file.");
+                bw.Dispose();
+            }
+        }
+
+    }
+}
