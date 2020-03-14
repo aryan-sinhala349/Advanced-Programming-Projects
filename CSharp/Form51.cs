16c792c00b2f790c0b9e82f60e4cb1ed183f2a7a
Aryan <46282971+aryan-sinhala349@users.noreply.github.com>
2020-03-14 16:30:58 -0400

    Added Form 51
    I almost typed Form 50, but then I used the Replace method to get Form 51.

---
+++ CSharp/Form51.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdvancedProgrammingCSharp
+{
+    public partial class Form51 : Form
+    {
+        public Form51()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string sentence = "Hello, my name is Adam. Hello!";
+            string after = sentence.Replace("Hello", "Hi");
+            MessageBox.Show(after, sentence);
+        }
+    }
+}
