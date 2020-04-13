49c160144941d06703139f0d186fae926870b4b2
Aryan <46282971+aryan-sinhala349@users.noreply.github.com>
2020-04-13 16:34:51 -0400

    Added Form 64
    I am now officially finished, and no longer have to do C# (until we start Unity)

---
+++ CSharp/Form64.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace AdvancedProgrammingCSharp
+{
+    public partial class Form64 : Form
+    {
+        public Form64()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (Process p in Process.GetProcessesByName("Chrome"))
+                MessageBox.Show(p.ProcessName);
+        }
+    }
+}
