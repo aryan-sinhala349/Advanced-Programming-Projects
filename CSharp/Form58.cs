93dad7008c0c1349a9d9880e55a86f0888e5f8a6
Aryan <46282971+aryan-sinhala349@users.noreply.github.com>
2020-04-13 15:43:31 -0400

    Added Form 58

---
+++ CSharp/Form58.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace AdvancedProgrammingCSharp
+{
+    public partial class Form58 : Form
+    {
+        FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+        public Form58()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (fbd.ShowDialog() == DialogResult.OK) MessageBox.Show(Directory.GetParent(fbd.SelectedPath).ToString());
+        }
+    }
+}
