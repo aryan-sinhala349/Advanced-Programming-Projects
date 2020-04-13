04e9607d8f2b3aafc5520283a3324ef677efad9b
Aryan <46282971+aryan-sinhala349@users.noreply.github.com>
2020-04-13 16:28:50 -0400

    Added Form 62
    All right then, fine. The title is "The Cheese Song". Ah-hem. Hn... Here I go. Pizza, mozzarella~ Pizza, mozzarella~ Rella rella rella rella Rella rella rella rella Rella rella rella rella Pizza, mozzarella~ ...And that's how it goes! What do you think? The second verse repeats with "Gorgonzola". Zola zola zola zola zola zola~ So? whaddaya think?

---
+++ CSharp/Form62.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace AdvancedProgrammingCSharp
+{
+    public partial class Form62 : Form
+    {
+        OpenFileDialog ofd = new OpenFileDialog();
+
+        public Form62()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (ofd.ShowDialog() == DialogResult.OK) MessageBox.Show(Path.HasExtension(ofd.FileName).ToString());
+        }
+    }
+}
