b2cf29d7af8f41ac73d011df264d6cd8fb705898
Aryan <46282971+aryan-sinhala349@users.noreply.github.com>
2020-04-13 15:32:11 -0400

    Added Forms 55 and 56.
    Been sticking to C++ lately... It's kind of nice to actually understand what I'm doing and getting error messages that make sense (not that these gave me any errors of course).

---
+++ CSharp/Form56.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdvancedProgrammingCSharp
+{
+    public partial class Form56 : Form
+    {
+        FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+        public Form56()
+        {
+            InitializeComponent();
+            fbd.Description = "Select a folder to see the path";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (fbd.ShowDialog() == DialogResult.OK) MessageBox.Show(fbd.SelectedPath);
+        }
+    }
+}
