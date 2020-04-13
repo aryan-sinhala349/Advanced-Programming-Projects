952d5926b0aedfa6f389b51d2d40d9db991a385f
Aryan <46282971+aryan-sinhala349@users.noreply.github.com>
2020-04-13 16:00:32 -0400

    Added Form 60
    Be careful! This one will delete any file that you tell it to delete.

---
+++ CSharp/Form60.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace AdvancedProgrammingCSharp
+{
+    public partial class Form60 : Form
+    {
+        OpenFileDialog ofd = new OpenFileDialog();
+
+        public Form60()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (ofd.ShowDialog() == DialogResult.OK) File.Delete(ofd.FileName);
+        }
+    }
+}
