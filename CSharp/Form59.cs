2e3093025553f35a70298ff54b23046fb6c26095
Aryan <46282971+aryan-sinhala349@users.noreply.github.com>
2020-04-13 15:48:08 -0400

    Added Form 59
    5 more tutorials to go! Also, be careful with this one because it deletes empty directories from a specified path.

---
+++ CSharp/Form59.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace AdvancedProgrammingCSharp
+{
+    public partial class Form59 : Form
+    {
+        FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+        public Form59()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                Directory.Delete(fbd.SelectedPath);
+                MessageBox.Show("Deleted \'" + fbd.SelectedPath + "\'");
+            }
+        }
+    }
+}
